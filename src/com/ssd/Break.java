56e0abb088e415aa9606ca7e025d2d62a64325e6
sirishaMarna <139740767+sirishaMarna@users.noreply.github.com>
2024-09-18 14:56:41 +0530

    Update Break.java
    adding the extra line for this

--- src/com/ssd/Break.java
+++ src/com/ssd/Break.java
@@ -3,6 +3,7 @@ package com.ssd;
 public class Break {
 	public static void main(String[] args) {
 		int n = 25;
+		System.out.println("just testing the task");
 		for (int i = 0; i <= n; i++) {
 
 			if (i == 7) {
